7ced3485e1419a2a53a6627dbfcfa3fd815b1448
Evgenii <emeleshkov@mail.ru>
2021-06-05 12:28:38 +0300

    Unique Char in String

--- Contents.swift
+++ Contents.swift
@@ -3,4 +3,23 @@ import UIKit
 var greeting = "Hello, playground"
 
 
-//Tasks on swift....Algorithms etc.  
+//Tasks on swift....Algorithms etc.
+
+
+//1. check if Characters are unique in a string
+
+func check (input: String) -> Bool {
+    var checkedChar = [Character]()
+    
+    for character in input {
+        if checkedChar.contains(character) {
+            return false
+        }
+        checkedChar.append(character)
+    }
+    return true
+}
+
+check(input: "Hello!")
+check(input: "1poijnu v92")
+//_____________________________________________
